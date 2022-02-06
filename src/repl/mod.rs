12240364c177f18c680f842f08f9447225e70c19
ymdarake <sakamoto.takumi.dev@gmail.com>
2022-02-06 21:09:27 +0900

    :sparkle: part06: Add REPL

---
+++ src/repl/mod.rs
@@ -0,0 +1,49 @@
+pub use crate::vm::VM;
+use std;
+use std::io;
+use std::io::Write;
+
+pub struct REPL {
+    command_buffer: Vec<String>,
+    vm: VM,
+}
+
+impl REPL {
+    pub fn new() -> REPL {
+        REPL {
+            vm: VM::new(),
+            command_buffer: vec![],
+        }
+    }
+
+    pub fn run(&mut self) {
+        println!("Welcome to Iridium! Let's be productive!");
+        loop {
+            let mut buffer = String::new();
+            let stdin = io::stdin();
+            print!(">>> ");
+            io::stdout().flush().expect("Unable to flush stdout");
+            stdin
+                .read_line(&mut buffer)
+                .expect("Unable to read line from user");
+            let buffer = buffer.trim();
+
+            self.command_buffer.push(buffer.to_string());
+
+            match buffer {
+                ".quit" => {
+                    println!("Farewell! Have a great day!");
+                    std::process::exit(0);
+                }
+                ".history" => {
+                    for command in &self.command_buffer {
+                        println!("{}", command);
+                    }
+                }
+                _ => {
+                    println!("Invalid input");
+                }
+            }
+        }
+    }
+}
